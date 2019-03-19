8318157ebbb8be0dec141d19d3500df78af4b592
Eduardo Backes <e.backes2011@gmail.com>
2019-03-19 20:50:51 -0300

    Engine - Migrations

---
+++ engine/migrations/001-initial.sql
@@ -0,0 +1,51 @@
+--------------------------------------------------------------------------------
+-- Up
+--------------------------------------------------------------------------------
+
+CREATE TABLE User (
+  id       INTEGER PRIMARY KEY,
+  name     TEXT    NOT NULL,
+  login    TEXT    NOT NULL,
+  password TEXT
+);
+
+CREATE TABLE Client (
+  id INTEGER PRIMARY KEY,
+  userId INTEGER NOT NULL,
+  name TEXT NOT NULL,
+  isActive NUMERIC NOT NULL DEFAULT 1,
+  CONSTRAINT Client_fk_userId FOREIGN KEY (userId)
+    REFERENCES User (id) ON UPDATE CASCADE ON DELETE CASCADE
+  CONSTRAINT Client_ck_isActive CHECK (isActive IN (0, 1))
+);
+
+CREATE TABLE Project (
+  id INTEGER PRIMARY KEY,
+  clientId INTEGER NOT NULL,
+  name TEXT NOT NULL,
+  isFinished NUMERIC NOT NULL DEFAULT 0,
+  amountHours NUMERIC,
+  CONSTRAINT Project_fk_clientId FOREIGN KEY (clientId)
+    REFERENCES Client (id) ON UPDATE CASCADE ON DELETE CASCADE
+  CONSTRAINT Project_ck_isFinished CHECK (isFinished IN (0, 1))
+);
+
+CREATE INDEX Client_ix_userId ON Client (userId);
+
+CREATE INDEX Project_ix_clientId on Project (clientId);
+
+INSERT INTO User (id, name, login, password) VALUES (1, 'TEST USER', 'test', '123123');
+
+INSERT INTO Client (id, userId, name, isActive) VALUES (1, 1, 'TEST CLIENT', 1);
+
+INSERT INTO Project (id, clientId, name, isFinished) VALUES (1, 1, 'TEST PROJECT', 0);
+
+--------------------------------------------------------------------------------
+-- Down
+--------------------------------------------------------------------------------
+
+DROP INDEX Project_ix_clientId;
+DROP INDEX Client_ix_userId;
+DROP TABLE Project;
+DROP TABLE Client;
+DROP TABLE User;
