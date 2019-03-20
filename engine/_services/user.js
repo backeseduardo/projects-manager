ab77249b36eebea4da47bbeafbb0a9b2713768df
Eduardo Backes <e.backes2011@gmail.com>
2019-03-19 21:40:12 -0300

    User service with all crud methods

---
+++ engine/_services/user.js
@@ -0,0 +1,13 @@
+module.exports = (db) => ({
+
+  getAll: () => db.all('SELECT * FROM User'),
+
+  getById: id => db.get('SELECT * FROM User WHERE id = ?', id),
+
+  insert: (user) => db.run('INSERT INTO User (name, login, password) VALUES (?, ?, ?)', user.name, user.login, user.password),
+
+  update: (user) => db.run('UPDATE User SET name = ? WHERE id = ?', user.name, user.id),
+  
+  delete: (id) => db.run('DELETE FROM User WHERE id = ?', id)
+
+})
