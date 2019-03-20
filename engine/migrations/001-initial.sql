69224b702b8925806ace2ed99cc38bff6da92532
Eduardo Backes <e.backes2011@gmail.com>
2019-03-19 21:22:49 -0300

    Added autoincrement to migrations

--- engine/migrations/001-initial.sql
+++ engine/migrations/001-initial.sql
@@ -3,16 +3,16 @@
 --------------------------------------------------------------------------------
 
 CREATE TABLE User (
-  id       INTEGER PRIMARY KEY,
+  id       INTEGER PRIMARY KEY AUTOINCREMENT,
   name     TEXT    NOT NULL,
   login    TEXT    NOT NULL,
   password TEXT
 );
 
 CREATE TABLE Client (
-  id INTEGER PRIMARY KEY,
-  userId INTEGER NOT NULL,
-  name TEXT NOT NULL,
+  id       INTEGER PRIMARY KEY AUTOINCREMENT,
+  userId   INTEGER NOT NULL,
+  name     TEXT    NOT NULL,
   isActive NUMERIC NOT NULL DEFAULT 1,
   CONSTRAINT Client_fk_userId FOREIGN KEY (userId)
     REFERENCES User (id) ON UPDATE CASCADE ON DELETE CASCADE
@@ -20,10 +20,10 @@ CREATE TABLE Client (
 );
 
 CREATE TABLE Project (
-  id INTEGER PRIMARY KEY,
-  clientId INTEGER NOT NULL,
-  name TEXT NOT NULL,
-  isFinished NUMERIC NOT NULL DEFAULT 0,
+  id          INTEGER PRIMARY KEY AUTOINCREMENT,
+  clientId    INTEGER NOT NULL,
+  name        TEXT    NOT NULL,
+  isFinished  NUMERIC NOT NULL DEFAULT 0,
   amountHours NUMERIC,
   CONSTRAINT Project_fk_clientId FOREIGN KEY (clientId)
     REFERENCES Client (id) ON UPDATE CASCADE ON DELETE CASCADE
